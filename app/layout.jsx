51971533038259fe47527a776402b9d0672d7a71
Sebastian Medina <39243497+sebapch@users.noreply.github.com>
2023-01-27 17:32:09 -0300

    test v1

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,13 +3,10 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head>
         <title>Como se hace?</title>
       </head>
+      
       <body  className='bg-black'>{children}</body>
     </html>
   )
